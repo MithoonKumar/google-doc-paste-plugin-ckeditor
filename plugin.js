2edac61d1eb86723808059dceb0deccd40279972
tikTokTik <mithoonk@flock.com>
2018-08-07 01:26:17 +0530

    Use only vanilla javscript

--- plugin.js
+++ plugin.js
@@ -4,7 +4,7 @@
         requires: 'clipboard',
         init: function (editor) {
             editor.on('paste', function (evt) {
-                if ("data" in evt && "dataValue" in evt.data && evt.data.dataValue.includes("docs-internal-guid") && "dataTransfer" in evt.data && "_" in evt.data.dataTransfer && "data" in evt.data.dataTransfer._ && "text/html" in evt.data.dataTransfer._.data) {
+                if ("data" in evt && "dataValue" in evt.data && evt.data.dataValue.indexOf("docs-internal-guid") > -1 && "dataTransfer" in evt.data && "_" in evt.data.dataTransfer && "data" in evt.data.dataTransfer._ && "text/html" in evt.data.dataTransfer._.data) {
                     evt.data.dataValue = formatting(evt.data.dataTransfer._.data['text/html']);
                 }
             });
@@ -13,53 +13,58 @@
 })();
 
 function recursiveFormatting(context) {
-    var children = context.get(0).children;
-    var index = 0;
-    context.children().each(function () {
-        var clonedEle;
-        //for italicity
-        if (children[index].style.fontStyle !== "") {
-            if (children[index].style.fontStyle === "italic") {
-                children[index].style.fontStyle = null;
-                clonedEle = $("<em></em>").append($(children[index]).clone());
-                $(children[index]).replaceWith(clonedEle.get(0));
+    for(let index = 0; index < context.childNodes.length; index++){
+      //for italicity
+        if (context.childNodes[index].style.fontStyle !== "") {
+            if (context.childNodes[index].style.fontStyle === "italic") {
+                context.childNodes[index].style.fontStyle = null;
+                let clonedEle = context.childNodes[index].cloneNode(true);
+                let ele = document.createElement("em");
+                ele.append(clonedEle)
+                context.replaceChild(ele, context.childNodes[index]);
             } else {
-                children[index].style.fontStyle = null;
+                context.childNodes[index].style.fontStyle = null;
             }
         }
      //for underline
-        if (children[index].style.textDecoration !== "") {
-            if (children[index].style.textDecoration === "underline") {
-                children[index].style.textDecoration = null;
-                clonedEle = $("<u></u>").append($(children[index]).clone());
-                $(children[index]).replaceWith(clonedEle.get(0));
+        if (context.childNodes[index].style.textDecoration !== "") {
+            if (context.childNodes[index].style.textDecoration === "underline") {
+                context.childNodes[index].style.textDecoration = null;
+                let clonedEle = context.childNodes[index].cloneNode(true);
+                let ele = document.createElement("u");
+                ele.append(clonedEle)
+                context.replaceChild(ele, context.childNodes[index]);
             } else {
-                children[index].style.textDecoration = null;
+                context.childNodes[index].style.textDecoration = null;
             }
         }
        //for bold
-        if (children[index].style.fontWeight !== "") {
-            if (children[index].style.fontWeight > "400") {
-                children[index].style.fontWeight = null;
-                clonedEle = $("<strong></strong>").append($(children[index]).clone());
-                $(children[index]).replaceWith(clonedEle.get(0));
+        if (context.childNodes[index].style.fontWeight !== "") {
+            if (context.childNodes[index].style.fontWeight > "400") {
+                context.childNodes[index].style.fontWeight = null;
+                let clonedEle = context.childNodes[index].cloneNode(true);
+                let ele = document.createElement("strong");
+                ele.append(clonedEle)
+                context.replaceChild(ele, context.childNodes[index]);
             } else {
-                children[index].style.fontWeight = null;
+                context.childNodes[index].style.fontWeight = null;
             }
         }
-        if ($(children[index]).children().length > 0) {
-            recursiveFormatting($(children[index]));
+        if (context.childNodes[index].children.length > 0) {
+            recursiveFormatting(context.childNodes[index]);
         }
-        index++;
-    });
+    }
 }
 
+
 function formatting(str) {
-    var jqueryObject = $(str);
-    var parent = $("<div></div>");
-    jqueryObject.children().each(function () {
-        parent.append($(this));
-    });
+    var parent = document.createElement("div");
+    var parentCopy = document.createElement("div");
+    parentCopy.innerHTML = str;
+    var len = parentCopy.childNodes[0].children.length;
+    for(let i = 0; i<len; i++ ) {
+        parent.appendChild(parentCopy.childNodes[0].children[i].cloneNode(true));
+    }
     recursiveFormatting (parent);
-    return parent.get(0).innerHTML;
+    return parent.innerHTML;
 }
